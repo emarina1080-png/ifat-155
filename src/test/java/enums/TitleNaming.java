aafd380576bc8ccd3a3ebac5b5be3242a855bfd3
Marinochka <emarina1080@gmail.com>
2025-12-07 15:15:59 +0300

    Added: Allure, annotations, enum, Lombok, remove empty lines

---
+++ src/test/java/enums/TitleNaming.java
@@ -0,0 +1,13 @@
+package enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public enum TitleNaming {
+    PRODUCTS("Products"),
+    CARTS("Your Cart"),
+    CHECKOUT("Checkout: Your Information");
+    private final String displayName;
+}
--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,48 +1,60 @@
 package tests;
 
+import io.qameta.allure.*;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+import user.User;
+import user.UserFactory;
 
+import static enums.TitleNaming.PRODUCTS;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
+import static user.UserFactory.withAdminPermission;
 
 public class LoginTest extends BaseTest {
-
     @DataProvider()
     public Object[][] loginData() {
         return new Object[][]{
-                {locked_user, password, "Epic sadface: Sorry, this user has been locked out."},
-                /* {"", "secret_sauce", "Epic sadface: Username is required"},
-                 {"standard_user", "", "Epic sadface: Password is required"},
-                 {"Locked_out_user", "secret_sauce", "Epic sadface: Username and password do not match any user in this service"}
-         */
+                /*      {PropertyReader.getProperty("saucedemo.locked_user"),
+                              PropertyReader.getProperty("saucedemo.password"),
+                              "Epic sadface: Sorry, this user has been locked out."},*/
+
+                {UserFactory.withLockedUserPermission(),
+                        "Epic sadface: Sorry, this user has been locked out."},
+
+                {UserFactory.withUsernameOnly(""),
+                        "Epic sadface: Username is required"},
+
+                {UserFactory.withPasswordOnly(""),
+                        "Epic sadface: Password is required"},
+
+                {new User("Locked_out_user", "secret_sauce"),
+                        "Epic sadface: Username and password do not match any user in this service"}
         };
-
     }
 
-    @Test(description = "Проверка корректного логина", priority = 1, dataProvider = "loginData")
-    public void checkIncorrectLogin(String user, String password, String errorMsg) {
-        //открыть брайзре
-        //зайти на сайт
-        System.out.println("LoginTest inc is runing in thread: " + Thread.currentThread().getId());
-
+    @Epic("Создание лида")
+    @Feature("Создание с карточки клиента")
+    @Story("Лангинация")
+    @TmsLink("ifat-155")
+    @Severity(SeverityLevel.BLOCKER)
+    @Owner("Eshkeeva Marina emarina1080@gmail.com")
+    @Issue("ifat-155")
+    @Test(description = "Проверка некорректного логина", priority = 1, dataProvider = "loginData")
+    public void checkIncorrectLogin(User user, String errorMsg) {
         loginPage.open();
-        loginPage.login(user, password);
-        //Thread.sleep(9000);
-        //boolean isVisible = loginPage.isErrorMsgAppear(); // если переменная повтряется несколько раз в кода, лучше выносить таким образом
-        assertTrue(loginPage.isErrorMsgAppear(), "Error: message does not appear");
+        loginPage.login(user);
+        assertTrue(loginPage.isErrorMsgAppear(), "Error message does not appear");
         assertEquals(loginPage.errorMsgText(), errorMsg);
-
     }
 
-    @Test(priority = 2, enabled = true, invocationCount = 2)
+    @Test(priority = 2, enabled = true, invocationCount = 1, alwaysRun = true)
     public void checkCorrectLogin() {
-        System.out.println("LoginTest correct is runing in thread: " + Thread.currentThread().getId());
+        System.out.println("LoginTest corr is running in thread: " + Thread.currentThread().getId());
 
         loginPage.open();
-        loginPage.login(user, password);
+        loginPage.login(withAdminPermission());
 
-        assertTrue(productsPage.isPageLoaded("Products"), "Error: Register button is not visible");
+        assertTrue(productsPage.isPageLoaded(PRODUCTS.getDisplayName()), "Register btn is not visible");
     }
-
 }
